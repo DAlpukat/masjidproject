a0d4ff98efaaff3169f32119b69597f57aae0f58
DAlpukat <justdgoo@gmail.com>
2026-02-09 13:22:27 +0800

    hehe

--- routes/web.php
+++ routes/web.php
@@ -9,71 +9,109 @@ use App\Http\Controllers\PageController;
 use App\Http\Controllers\LaporanKasController;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\KasController;
+use App\Http\Controllers\SuperAdminController;
 
-// --- 1. Landing Page ---
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// --- 1. Landing Page (Public) ---
 Route::get('/', function () {
     return view('welcome');
 })->name('welcome');
 
-// --- 2. Halaman User Biasa ---
+// --- 2. Authenticated Users (General) ---
 Route::middleware('auth')->group(function () {
-    Route::post('/room/{tempat}/leave', [HomeController::class, 'leave'])->name('room.leave');
+    // Home & Room Navigation
     Route::get('/home', [HomeController::class, 'index'])->name('home');
     Route::post('/join', [HomeController::class, 'join'])->name('join.store');
     Route::post('/join-public/{place}', [HomeController::class, 'joinPublic'])->name('join.public');
     Route::get('/my-rooms', [HomeController::class, 'myRooms'])->name('user.rooms');
     Route::get('/room/{slug}', [HomeController::class, 'viewRoom'])->name('room.view');
+    Route::post('/room/{tempat}/leave', [HomeController::class, 'leave'])->name('room.leave');
+
+    // Profile Management
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-// --- 3. Sistem Kas & Post ---
+// --- 3. Sistem Kas (Authenticated) ---
 Route::middleware('auth')->group(function () {
     Route::get('/room/{tempatId}/kas', [KasController::class, 'dashboard'])->name('kas.dashboard');
     Route::post('/room/{tempatId}/kas', [KasController::class, 'store'])->name('kas.store');
     Route::post('/room/{tempatId}/kategori', [KasController::class, 'storeKategori'])->name('kas.kategori.store');
     Route::delete('/kas/destroy/{id}', [KasController::class, 'destroy'])->name('kas.destroy');
+    
+    // Public Post View
     Route::get('/post/{id}', [PostController::class, 'show'])->name('post.show');
 });
 
-// --- 4. Halaman Admin (Hanya Admin) ---
-Route::middleware(['auth', 'admin'])->group(function () {
-    
-    // Dashboard Admin
-    Route::get('/admin/dashboard', [AdminController::class, 'index'])->name('admin.dashboard');
+// --- 4. Admin Routes (Khusus Admin Pemilik Kelas) ---
+// Menggunakan Prefix 'admin' dan Name 'admin.'
+Route::middleware(['auth', 'admin'])
+    ->prefix('admin')
+    ->name('admin.')
+    ->group(function () {
+        
+        // Dashboard Admin
+        Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
 
-    // Tempat Layanan
-    Route::get('/admin/tempat-layanan/create', [TempatLayananController::class, 'create'])->name('admin.temp.create');
-    Route::post('/admin/tempat-layanan', [TempatLayananController::class, 'store'])->name('admin.temp.store');
-    Route::get('/admin/tempat-layanan/{id}/edit', [TempatLayananController::class, 'edit'])->name('admin.temp.edit');
-    Route::put('/admin/tempat-layanan/{id}', [TempatLayananController::class, 'update'])->name('admin.temp.update');
-    Route::delete('/admin/tempat-layanan/{tempat}', [TempatLayananController::class, 'destroy'])->name('admin.temp.destroy');
-    
-    // Anggota Komunitas
-    Route::get('/admin/tempat-layanan/{id}/members', [TempatLayananController::class, 'members'])->name('admin.group.members');
-    Route::delete('/admin/tempat/{id}/members/{userId}', [TempatLayananController::class, 'kick'])->name('admin.members.kick');
+        // CRUD Tempat Layanan
+        Route::get('/tempat-layanan/create', [TempatLayananController::class, 'create'])->name('temp.create');
+        Route::post('/tempat-layanan', [TempatLayananController::class, 'store'])->name('temp.store');
+        Route::get('/tempat-layanan/{id}/edit', [TempatLayananController::class, 'edit'])->name('temp.edit');
+        Route::put('/tempat-layanan/{id}', [TempatLayananController::class, 'update'])->name('temp.update');
+        Route::delete('/tempat-layanan/{tempat}', [TempatLayananController::class, 'destroy'])->name('temp.destroy');
+        
+        // Manajemen Anggota
+        Route::get('/tempat-layanan/{id}/members', [TempatLayananController::class, 'members'])->name('group.members');
+        
+        // Rute 'Kick Member' (Diambil dari file pertama dan disesuaikan struktur prefix-nya)
+        Route::delete('/tempat/{id}/members/{userId}', [TempatLayananController::class, 'kick'])->name('members.kick');
 
-    // Pages Management (PERBAIKAN: Menambahkan 'admin.' pada name)
-    Route::get('/pages/{id}', [PageController::class, 'index'])->name('admin.pages.index');
-    Route::post('/pages', [PageController::class, 'store'])->name('admin.pages.store');
-    Route::get('/pages/{page}/edit', [PageController::class, 'edit'])->name('admin.pages.edit');
-    Route::put('/pages/{page}', [PageController::class, 'update'])->name('admin.pages.update');
-    Route::delete('/pages/{id}', [PageController::class, 'destroy'])->name('admin.pages.destroy');
-    
-    // Posts Management (PERBAIKAN: Menambahkan 'admin.' pada name)
-    Route::get('/pages/{pageId}/posts', [PostController::class, 'index'])->name('admin.posts.index');
-    Route::get('/pages/{pageId}/posts/create', [PostController::class, 'create'])->name('admin.posts.create');
-    Route::post('/posts/store', [PostController::class, 'store'])->name('admin.posts.store');
-    Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('admin.posts.destroy');
+        // Pages Management
+        Route::get('/pages/{id}', [PageController::class, 'index'])->name('pages.index');
+        Route::post('/pages', [PageController::class, 'store'])->name('pages.store');
+        Route::get('/pages/{page}/edit', [PageController::class, 'edit'])->name('pages.edit');
+        Route::put('/pages/{page}', [PageController::class, 'update'])->name('pages.update');
+        Route::delete('/pages/{id}', [PageController::class, 'destroy'])->name('pages.destroy');
+        
+        // Posts Management
+        Route::get('/pages/{pageId}/posts', [PostController::class, 'index'])->name('posts.index');
+        Route::get('/pages/{pageId}/posts/create', [PostController::class, 'create'])->name('posts.create');
+        Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+        Route::delete('/posts/{id}', [PostController::class, 'destroy'])->name('posts.destroy');
 });
 
-// --- 5. Route Standard Lainnya ---
+// --- 5. SuperAdmin Routes (Khusus SuperAdmin) ---
+// Menggunakan Prefix 'superadmin' dan Name 'superadmin.'
+Route::middleware(['auth', 'is.superadmin'])
+    ->prefix('superadmin')
+    ->name('superadmin.')
+    ->group(function () {
+        Route::get('/dashboard', [SuperAdminController::class, 'index'])->name('dashboard');
+        
+        // Approve Kelas (Pending -> Aktif)
+        Route::post('/tempat/{id}/approve', [SuperAdminController::class, 'approveTempat'])->name('approve');
+        
+        // Hapus Kelas Global
+        Route::delete('/tempat/{id}', [SuperAdminController::class, 'destroyTempat'])->name('destroy.tempat');
+        
+        // Hapus User Global
+        Route::delete('/user/{id}', [SuperAdminController::class, 'destroyUser'])->name('destroy.user');
+});
+
+// --- 6. Default Redirect ---
 Route::get('/dashboard', function () {
     return redirect()->route('home');
-})->middleware(['auth']);
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+})->middleware(['auth'])->name('dashboard');
 
 require __DIR__.'/auth.php';
